3fb6fa733a61ea2310dd5aa33079733a3d731952
Paper-Hat <justinkristian@gmail.com>
2017-11-07 22:47:26 -0800

    Minor edits
    .

---
+++ GGO_2017/Assets/Scripts/Parent Object/pEnemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*Enemy Base Class 
+    Enemy is an actor object*/
+public class pEnemy : MonoBehaviour {
+
+    public int strength; //Unique value for enemy that determines player fatigue over time
+	
+
+    //init
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
