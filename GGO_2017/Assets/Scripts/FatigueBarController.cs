260ec9452b1155781cbf046f3ab2470b4f41a41d
Paper-Hat <justinkristian@gmail.com>
2017-11-16 09:47:49 -0800

    Fatigue
    added basic fatigue system, no current implementation

---
+++ GGO_2017/Assets/Scripts/FatigueBarController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class FatigueBarController : MonoBehaviour {
+    public float fatigue;
+    public float minFatigue;
+    public float maxFatigue;
+    private Slider fatigueBar;
+	// Use this for initialization
+	void Start () {
+        fatigueBar = GetComponent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateFatigueBar();
+
+        fatigue = Mathf.Clamp(fatigue, minFatigue, maxFatigue);
+	}
+
+    void UpdateFatigueBar(){
+        fatigueBar.value = fatigue;
+    }
+}
