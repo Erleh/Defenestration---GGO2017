08bf0657dc7035ab86917d0e92314a2b9dc7b759
Erleh <eric.lee0051@gmail.com>
2017-11-28 08:55:18 -0800

    Minor Win Condition Fix
    Changes trigger type of win trigger

--- GGO_2017/Assets/WinCondition.cs
+++ GGO_2017/Assets/WinCondition.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class WinCondition : MonoBehaviour {
 
+	/*
 	void OnTriggerEnter2D(Collider2D col)
     {
         //Debug.Log("hit1");
@@ -17,4 +18,19 @@ public class WinCondition : MonoBehaviour {
             }
         }
     }
+    */
+
+	void OnTriggerStay2D(Collider2D col) 
+	{
+		Debug.Log("hit1");
+		if (col.GetComponent<Guard>() != null)
+		{
+			Debug.Log("hit2");
+			if (col.GetComponent<Guard>().pushing || col.GetComponent<Guard>().shoving || col.GetComponent<Guard>().kicking)
+			{
+				Debug.Log("hit3");
+				col.GetComponent<Guard>().Win();
+			}
+		}
+	}
 }
