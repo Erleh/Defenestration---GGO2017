20a25cf34bd0417ed3c7b69c160e0f37f321f2f6
Paper-Hat <justinkristian@gmail.com>
2017-11-29 09:33:50 -0800

    Character Tracker
    Added player progress tracker and title controls image to title screen

---
+++ GGO_2017/Assets/Scripts/Visuals_and_UI/Progress/ProgressController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ProgressController : MonoBehaviour {
+
+    public GameObject player;
+    public ProgressBarController progBar;
+    private void Awake()
+    {
+        progBar.progress = player.transform.localPosition.x;
+    }
+    void FixedUpdate()
+    {
+        progBar.progress = player.transform.localPosition.x;
+        Debug.Log(progBar.progress);
+    }
+
+}
--- GGO_2017/Assets/Scripts/Visuals_and_UI/TitleScreen/UIPanelFlash.cs
+++ GGO_2017/Assets/Scripts/Visuals_and_UI/TitleScreen/UIPanelFlash.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIPanelFade : MonoBehaviour {
+public class UIPanelFlash : MonoBehaviour {
 
     // Use this for initialization
     public GameObject panel;
