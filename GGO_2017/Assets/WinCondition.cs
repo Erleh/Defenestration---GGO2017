31998680e4f765795b1b01fc36578a785bb3a447
Erleh <eric.lee0051@gmail.com>
2017-11-30 23:09:26 -0800

    ADDS AWESOME KNOCKBACKS

--- GGO_2017/Assets/WinCondition.cs
+++ GGO_2017/Assets/WinCondition.cs
@@ -4,31 +4,15 @@ using UnityEngine;
 
 public class WinCondition : MonoBehaviour {
 
-	/*
-	void OnTriggerEnter2D(Collider2D col)
-    {
-        //Debug.Log("hit1");
-        if (col.GetComponent<Guard>() != null)
-        {
-            //Debug.Log("hit2");
-            if (col.GetComponent<Guard>().pushing || col.GetComponent<Guard>().shoving || col.GetComponent<Guard>().kicking)
-            {
-                //Debug.Log("hit3");
-                col.GetComponent<Guard>().Win();
-            }
-        }
-    }
-    */
-
 	void OnTriggerStay2D(Collider2D col) 
 	{
-		Debug.Log("hit1");
+		//Debug.Log("hit1");
 		if (col.GetComponent<Guard>() != null)
 		{
-			Debug.Log("hit2");
-			if (col.GetComponent<Guard>().pushing || col.GetComponent<Guard>().shoving || col.GetComponent<Guard>().kicking)
+			//Debug.Log("hit2");
+			if (col.GetComponent<Guard>().pushing || col.GetComponent<Guard>().shoving || col.GetComponent<Guard>().kicking || col.GetComponent<Guard>().grapple)
 			{
-				Debug.Log("hit3");
+				//Debug.Log("hit3");
 				col.GetComponent<Guard>().Win();
 			}
 		}
