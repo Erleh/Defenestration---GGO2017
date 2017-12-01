11a8fa5e9337a1796a31c52b0174949698e61b77
Michael Zatlin <michael.zatlin@yahoo.com>
2017-12-01 04:12:46 -0800

    Fade out of title screen
    when you press space, the title screen fades to black
    also you can spam the menu sound when pressing space

--- GGO_2017/Assets/Scripts/Visuals_and_UI/TitleScreen/GameLoader.cs
+++ GGO_2017/Assets/Scripts/Visuals_and_UI/TitleScreen/GameLoader.cs
@@ -9,23 +9,38 @@ public class GameLoader : MonoBehaviour {
     //sounds
     public AudioSource buttonPress;
     public AudioSource startGame;
+    public CanvasGroup canvasgroup;
+    bool ispressed = false;
+
 
     public IEnumerator LoadGame()
     {
-        startGame.Play();
+       
 
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(1.5f);
         SceneManager.LoadScene("GameScene");
+    }
+    public IEnumerator FadeOut()
+    {
+        startGame.Play();
+        canvasgroup = GetComponent<CanvasGroup>();
+        while (canvasgroup.alpha > 0)
+        {
+            canvasgroup.alpha -= Time.deltaTime;
+            yield return null;
+        }
+        canvasgroup.interactable = false;
     }
 
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)&& !ispressed)
         {
             buttonPress.Play();
-
+            StartCoroutine(FadeOut());
             StartCoroutine(LoadGame());
+            ispressed = true;
             //Debug.Log("gets here");
             //SceneManager.LoadScene("GameScene");
         }
