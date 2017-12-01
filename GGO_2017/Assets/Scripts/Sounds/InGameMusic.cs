49a2ba49ac9f24ac1c954ec58d6fba3be82aa264
Erleh <eric.lee0051@gmail.com>
2017-12-01 06:38:34 -0800

    ADDITIONS TO MUSSIIICCC AND OTHER FIXES

---
+++ GGO_2017/Assets/Scripts/Sounds/InGameMusic.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameMusic : MonoBehaviour
+{
+    public AudioSource inGameMusicV1;
+
+    void Start()
+    {
+        inGameMusicV1.Play();
+    }
+}
